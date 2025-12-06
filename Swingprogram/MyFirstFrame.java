ed6d4d2cd1a6123c52e14896185db5855219e614
Palak Agarwal <agarwalpalak612@gmail.com>
2025-12-06 23:17:13 +0530

    Initial commit for Java Swing project

---
+++ Swingprogram/MyFirstFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class MyFristFrame{
+    public static void main(String args[]){
+        JFrame frame= new JFrame();
+        frame.setSize(300, 200);   // important to see the window
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
