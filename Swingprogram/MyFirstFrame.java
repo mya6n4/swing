2e7d1ee5deda4176c4281c25e526342deca7ea49
Palak Agarwal <agarwalpalak612@gmail.com>
2025-12-06 23:28:50 +0530

    Updated Swing login UI

--- Swingprogram/MyFirstFrame.java
+++ Swingprogram/MyFirstFrame.java
@@ -5,6 +5,6 @@ class MyFristFrame{
         frame.setSize(300, 200);   // important to see the window
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setLocation(100,50);
     }
 }
